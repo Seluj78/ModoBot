4500bceef857420fcb6bec6a2eb871d86009126b
Jules Lasne <jules.lasne@gmail.com>
2020-12-16 09:40:23 +0100

    Big optimisation on permissions check

--- modobot/utils/checks.py
+++ modobot/utils/checks.py
@@ -13,70 +13,7 @@ async def permissions_check(ctx):
     if not roleperms:
         await ctx.message.delete()
         raise UnauthorizedError("Vous n'êtes pas authorisé à utiliser cette commande.")
-    if ctx.command.name == "ban" and not roleperms.can_ban:
-        if not roleperms.silence_notif:
-            await ctx.message.delete()
-            await ctx.author.send(
-                f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-            )
-        raise UnauthorizedError(
-            f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-        )
-    elif ctx.command.name == "unban" and not roleperms.can_unban:
-        if not roleperms.silence_notif:
-            await ctx.message.delete()
-            await ctx.author.send(
-                f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-            )
-        raise UnauthorizedError(
-            f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-        )
-    elif ctx.command.name == "warn" and not roleperms.can_warn:
-        if not roleperms.silence_notif:
-            await ctx.message.delete()
-            await ctx.author.send(
-                f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-            )
-        raise UnauthorizedError(
-            f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-        )
-    elif ctx.command.name == "note" and not roleperms.can_note:
-        if not roleperms.silence_notif:
-            await ctx.message.delete()
-            await ctx.author.send(
-                f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-            )
-        raise UnauthorizedError(
-            f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-        )
-    elif ctx.command.name == "search" and not roleperms.can_search:
-        if not roleperms.silence_notif:
-            await ctx.message.delete()
-            await ctx.author.send(
-                f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-            )
-        raise UnauthorizedError(
-            f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-        )
-    elif ctx.command.name == "clear" and not roleperms.can_clear:
-        if not roleperms.silence_notif:
-            await ctx.message.delete()
-            await ctx.author.send(
-                f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-            )
-        raise UnauthorizedError(
-            f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-        )
-    elif ctx.command.name == "lock" and not roleperms.can_lock:
-        if not roleperms.silence_notif:
-            await ctx.message.delete()
-            await ctx.author.send(
-                f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-            )
-        raise UnauthorizedError(
-            f"Vous n'êtes pas autorisé à utiliser `{ctx.command.name}`"
-        )
-    elif ctx.command.name == "unlock" and not roleperms.can_unlock:
+    if not getattr(roleperms, "can_" + ctx.command.name, False):
         if not roleperms.silence_notif:
             await ctx.message.delete()
             await ctx.author.send(
