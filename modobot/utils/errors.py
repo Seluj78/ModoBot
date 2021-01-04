7cca7ca148cc7e76a5a7ef9d7b06fadf949d4498
Jules Lasne <jules.lasne@gmail.com>
2021-01-04 22:48:53 +0100

    Updated errors file to use a dict and not a elif forest

--- modobot/utils/errors.py
+++ modobot/utils/errors.py
@@ -85,104 +85,110 @@ class NotMutedError(commands.BadArgument):
         super().__init__(message)
 
 
-# TODO: Convert this big if forest to a dict
+error_dict = {
+    NotMutedError: {
+        "message": ":x: L'utilisateur n'est pas mute.",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+    AlreadyMuteError: {
+        "message": ":x: L'utilisateur est déjà mute.",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+    PunishStaffError: {
+        "message": ":x: Vous ne pouvez pas punir un autre membre du staff.",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+    PunishBotError: {
+        "message": ":x: Pourquoi me faire tant de mal, je ne suis qu'un bot :robot:.",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+    commands.MissingRequiredArgument: {
+        "message": ":x: Argument(s) requis manquant(s).",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+    commands.CommandNotFound: {
+        "message": ":x: Commande inconnue.",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+    commands.MemberNotFound: {
+        "message": ":grey_question: Utilisateur inconnu.",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+    UserAlreadyBannedError: {
+        "message": ":x: Cet utilisateur est déjà banni.",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+    UserNotBannedError: {
+        "message": ":x: Cet utilisateur n'est pas banni.",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+    IncorrectTimeError: {
+        "message": ":x: Format de temps incorrect.",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+    RoleAlreadyInSameCat: {
+        "message": ":x: Ce rôle est déjà dans cette catégorie.",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+    RoleAlreadyInCat: {
+        "message": ":x: Ce rôle est déjà dans une catégorie.",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+    RoleCatDoesntExist: {
+        "message": ":x: La catégorie demandée n'existe pas.",
+        "color": discord.Color.red(),
+        "send_to": "channel",
+    },
+}
 
 
 @modobot_client.event
 async def on_command_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.message.delete()
-        embed = discord.Embed(
-            description=":x: Argument(s) requis manquant(s)", color=discord.Color.red()
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, commands.CommandNotFound):
-        await ctx.message.delete()
-        embed = discord.Embed(
-            description=":x: Commande inconnue", color=discord.Color.red()
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, commands.MemberNotFound):
-        await ctx.message.delete()
-        embed = discord.Embed(
-            description=f":grey_question: {str(error)}", color=discord.Color.red()
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, PunishBotError):
-        await ctx.message.delete()
-        embed = discord.Embed(
-            description=f":x: {str(error)}", color=discord.Color.red()
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, NotMutedError):
-        embed = discord.Embed(
-            description=f":x: {str(error)}", color=discord.Color.red()
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, PunishStaffError):
-        await ctx.message.delete()
-        embed = discord.Embed(
-            description=f":x: {str(error)}", color=discord.Color.red()
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, AlreadyMuteError):
-        embed = discord.Embed(
-            description=f":x: {str(error)}", color=discord.Color.red()
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, UserAlreadyBannedError):
-        await ctx.message.delete()
-        embed = discord.Embed(
-            description=":x: Cet utilisateur est déjà banni.", color=discord.Color.red()
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, UserNotBannedError):
-        embed = discord.Embed(
-            description=f":x: {str(error)}", color=discord.Color.red()
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, UnauthorizedError):
-        embed = discord.Embed(
-            description=f":x: Vous n'êtes pas autorisé à utiliser {ctx.command.name}.",
-            color=discord.Color.red(),
-        )
-        embed.set_footer(
-            text="Contactez un administrateur si vous pensez que c'est une erreur."
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, RoleCatDoesntExist):
-        embed = discord.Embed(
-            description=":x: La catégorie demandée n'existe pas.",
-            color=discord.Color.red(),
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, RoleAlreadyInCat):
-        embed = discord.Embed(
-            description=":x: Le rôle est déjà dans une catégorie.",
-            color=discord.Color.red(),
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, RoleAlreadyInSameCat):
-        embed = discord.Embed(
-            description=":x: Le rôle est déjà dans cette catégorie.",
-            color=discord.Color.red(),
-        )
-        await ctx.channel.send(embed=embed)
-    elif isinstance(error, UnauthorizedChannelError):
-        embed = discord.Embed(
-            description=f":x: Vous n'êtes pas autorisé à utiliser {ctx.command.name} dans {ctx.channel.name}.",
-            color=discord.Color.red(),
-        )
-        embed.set_footer(
-            text="Contactez un administrateur si vous pensez que c'est une erreur."
-        )
-        await ctx.author.send(embed=embed)
-    elif isinstance(error, commands.CheckFailure):
-        logging.warning(f"Check failed: {error}")
-        await send_error_embed(ctx, f"Erreur de check: {error}", "Essayez a nouveau")
-    elif isinstance(error, IncorrectTimeError):
-        embed = discord.Embed(description=":x: Format incorrect")
-        await ctx.channel.send(embed=embed)
+    data = error_dict.get(type(error))
+    if data:
+        embed = discord.Embed(description=data["message"], color=data["color"])
+        if data["send_to"] == "channel":
+            await ctx.channel.send(embed=embed)
+        else:
+            await ctx.author.send(embed=embed)
     else:
-        logging.error(f"Unknow error {error}")
-        await send_error_embed(ctx, f"Erreur inconnue: {error}", "Essayez a nouveau")
+        if isinstance(error, UnauthorizedError):
+            embed = discord.Embed(
+                description=f":x: Vous n'êtes pas autorisé à utiliser {ctx.command.name}.",
+                color=discord.Color.red(),
+            )
+            embed.set_footer(
+                text="Contactez un administrateur si vous pensez que c'est une erreur."
+            )
+            await ctx.channel.send(embed=embed)
+        elif isinstance(error, UnauthorizedChannelError):
+            embed = discord.Embed(
+                description=f":x: Vous n'êtes pas autorisé à utiliser {ctx.command.name} dans {ctx.channel.name}.",
+                color=discord.Color.red(),
+            )
+            embed.set_footer(
+                text="Contactez un administrateur si vous pensez que c'est une erreur."
+            )
+            await ctx.author.send(embed=embed)
+        elif isinstance(error, commands.CheckFailure):
+            logging.warning(f"Check failed: {error}")
+            await send_error_embed(
+                ctx, f"Erreur de check: {error}", "Essayez a nouveau"
+            )
+        else:
+            logging.error(f"Unknow error {error}")
+            await send_error_embed(
+                ctx, f"Erreur inconnue: {error}", "Essayez a nouveau"
+            )
